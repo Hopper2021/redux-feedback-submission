ec2197426b7e27f427a6e711d0f513a27830fa8c
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-09-23 21:04:25 -0500

    Added classNames and base alignment css

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,26 +10,40 @@ function App() {
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
-      <div className="question-div">
-        <h1>How are you feeling today?</h1>
-        <input type="number" />
-        <button>Next</button>
-      </div>
-      <div className="question-div">
-        <h1>How well are you understanding the content?</h1>
-        <input type="number" />
-        <button>Next</button>
-      </div>
-      <div className="question-div">
-        <h1>How well are you being supported?</h1>
-        <input type="number" />
-        <button>Next</button>
-      </div>
-      <div className="question-div">
-        <h1>Any comments you want to leave?</h1>
-        <input type="text" />
-        <button>Next</button>
-      </div>
+      <form>
+        <div className="question-div">
+          <h1>How are you feeling today?</h1>
+          <div className="input-div">
+            <p>Feeling?</p>
+            <input type="number" />
+            <button>Next</button>
+          </div>
+        </div>
+        <div className="question-div">
+          <h1>How well are you understanding the content?</h1>
+          <div className="input-div">
+            <p>Understanding?</p>
+            <input type="number" />
+            <button>Next</button>
+          </div>
+        </div>
+        <div className="question-div">
+          <h1>How well are you being supported?</h1>
+          <div className="input-div">
+            <p>Support?</p>
+            <input type="number" />
+            <button>Next</button>
+          </div>
+        </div>
+        <div className="question-div">
+          <h1>Any comments you want to leave?</h1>
+          <div className="input-div">
+            <p>Comments?</p>
+            <input type="number" />
+            <button>Next</button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
