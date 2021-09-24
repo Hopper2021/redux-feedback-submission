9c7de95e9bd9865a338a5228ab1ebcbf97b82542
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-09-23 20:50:09 -0500

    Built bare bones html questions

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,6 +10,26 @@ function App() {
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+      <div className="question-div">
+        <h1>How are you feeling today?</h1>
+        <input type="number" />
+        <button>Next</button>
+      </div>
+      <div className="question-div">
+        <h1>How well are you understanding the content?</h1>
+        <input type="number" />
+        <button>Next</button>
+      </div>
+      <div className="question-div">
+        <h1>How well are you being supported?</h1>
+        <input type="number" />
+        <button>Next</button>
+      </div>
+      <div className="question-div">
+        <h1>Any comments you want to leave?</h1>
+        <input type="text" />
+        <button>Next</button>
+      </div>
     </div>
   );
 }
