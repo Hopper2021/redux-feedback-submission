ea4afc9fcb334a3f0ff2dd80a26f7816a568fe0c
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-09-23 21:18:54 -0500

    Added Review and thank you divs

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,36 +11,51 @@ function App() {
         <h4>Don't forget it!</h4>
       </header>
       <form>
-        <div className="question-div">
+        <div className="question-container">
           <h1>How are you feeling today?</h1>
           <div className="input-div">
-            <p>Feeling?</p>
+            <p className="input-question">Feeling?</p>
             <input type="number" />
-            <button>Next</button>
+            <button className="next-button">Next</button>
           </div>
         </div>
-        <div className="question-div">
+        <div className="question-container">
           <h1>How well are you understanding the content?</h1>
           <div className="input-div">
-            <p>Understanding?</p>
+            <p className="input-question">Understanding?</p>
             <input type="number" />
-            <button>Next</button>
+            <button className="next-button">Next</button>
           </div>
         </div>
-        <div className="question-div">
+        <div className="question-container">
           <h1>How well are you being supported?</h1>
           <div className="input-div">
-            <p>Support?</p>
+            <p className="input-question">Support?</p>
             <input type="number" />
-            <button>Next</button>
+            <button className="next-button">Next</button>
           </div>
         </div>
-        <div className="question-div">
+        <div className="question-container">
           <h1>Any comments you want to leave?</h1>
           <div className="input-div">
-            <p>Comments?</p>
+            <p className="input-question">Comments?</p>
             <input type="number" />
-            <button>Next</button>
+            <button className="next-button">Next</button>
+          </div>
+        </div>
+        <div className="review-container">
+          <h1>Review Your Feedback</h1>
+          <p>Feelings:</p>
+          <p>Understanding:</p>
+          <p>Support:</p>
+          <p>Comments:</p>
+          <button className="submit-button">Submit</button>
+        </div>
+        <div className="thank-you-container">
+          <h1>Feedback!</h1>
+          <div>
+            <h1 className="feedback-thank-you">Thank you!</h1>
+            <button>Leave New Feedback</button>
           </div>
         </div>
       </form>
