cd015cbb3e66c5982f13e3663c166692bf716c8e
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-09-25 10:48:37 -0500

    Added useSelector to submitPage

--- src/components/SubmitPage/SubmitPage.jsx
+++ src/components/SubmitPage/SubmitPage.jsx
@@ -1,11 +1,16 @@
+import { useSelector } from 'react-redux';
+
 function SubmitPage() {
+    const reduxStore = useSelector(store => store);
     return(
         <div className="review-container">
             <h1>Review Your Feedback</h1>
-            <p>Feelings:</p>
-            <p>Understanding:</p>
-            <p>Support:</p>
-            <p>Comments:</p>
+            <form>
+                <p>Feelings:</p>
+                <p>Understanding:</p>
+                <p>Support:</p>
+                <p>Comments:</p>
+            </form>
             <button className="submit-button">Submit</button>
         </div>
     )
