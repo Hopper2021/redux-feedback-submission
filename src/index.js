503cf45492667a15f83566f1f084ed62f18e543e
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-09-24 20:19:11 -0500

    Added redux, logger and combineReducers

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux'; 
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+
+const reducer = (state = [], action) => {
+    return state;
+}
+
+const storeInstance = createStore(
+    combineReducers({
+        reducer
+    }), 
+    applyMiddleware(logger),
+)
+
+ReactDOM.render(
+    <Provider store={storeInstance}>
+        <App />
+    </Provider>,
+document.getElementById('root'));
 registerServiceWorker();
