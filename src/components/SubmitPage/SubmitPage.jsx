edef871c568b316af1114cd378f8a1902df1a3c5
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-09-25 20:32:41 -0500

    Added POST to submitPage

--- src/components/SubmitPage/SubmitPage.jsx
+++ src/components/SubmitPage/SubmitPage.jsx
@@ -1,17 +1,34 @@
 import { useSelector } from 'react-redux';
+import axios from 'axios';
 
 function SubmitPage() {
-    const reduxStore = useSelector(store => store);
+    const answerList = useSelector(store => store.answerReducer);
+    // Might need to make a POST to the server 
+    // and a GET to display the information
+    const handleSubmit = (event) => {
+    event.preventDefault();
+
+    axios({
+        method: 'POST',
+        url: '/answers',
+        data: { answerList }
+        })
+        history.push('/thankYou')
+    }
+
     return(
         <div className="review-container">
             <h1>Review Your Feedback</h1>
-            <form>
-                <p>Feelings:</p>
-                <p>Understanding:</p>
-                <p>Support:</p>
-                <p>Comments:</p>
+            {/* {JSON.stringify(answerList)} */}
+            <form onSubmit={handleSubmit}>
+                <p>Feeling: {answerList[0]}</p>
+                <p>Understanding: {answerList[1]}</p>
+                <p>Support: {answerList[2]}</p>
+                <p>Comments: {answerList[3]}</p>
+                <button 
+                    type="submit"
+                    className="submit-button">Submit</button>
             </form>
-            <button className="submit-button">Submit</button>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -8,40 +8,47 @@ import logger from 'redux-logger';
 import { Provider } from 'react-redux'; 
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 
-const feelingReducer = (state = [], action) => {
+const answerReducer = (state = [], action) => {
     if (action.type === 'SET_FEELING') {
         return [...state, action.payload.feeling];
-    }
-    return state;
-}
-
-const understandingReducer = (state = [], action) => {
-    if (action.type === 'SET_UNDERSTANDING') {
+    } else if (action.type === 'SET_UNDERSTANDING'){
         return [...state, action.payload.understanding];
-    }
-    return state;
-}
-
-const supportReducer = (state = [], action) => {
-    if (action.type === 'SET_SUPPORT'){
+    } else if (action.type === 'SET_SUPPORT'){
         return [...state, action.payload.support];
-    }
-    return state;
-}
-
-const commentReducer = (state = [], action) => {
-    if (action.type === 'SET_COMMENT') {
+    } else if (action.type === 'SET_COMMENT') {
         return [...state, action.payload.comment];
     }
     return state;
 }
 
+// const understandingReducer = (state = '', action) => {
+//     if (action.type === 'SET_UNDERSTANDING') {
+//         return [...state, action.payload.understanding];
+//     }
+//     return state;
+// }
+
+// const supportReducer = (state = '', action) => {
+//     if (action.type === 'SET_SUPPORT'){
+//         return [...state, action.payload.support];
+//     }
+//     return state;
+// }
+
+// const commentReducer = (state = '', action) => {
+//     if (action.type === 'SET_COMMENT') {
+//         return [...state, action.payload.comment];
+//     }
+//     return state;
+// }
+
 const storeInstance = createStore(
     combineReducers({
-        feelingReducer,
-        understandingReducer,
-        supportReducer,
-        commentReducer
+        answerReducer
+        // feelingReducer,
+        // understandingReducer,
+        // supportReducer,
+        // commentReducer
     }), 
     applyMiddleware(logger),
 )
